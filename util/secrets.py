98319d6fc8c5fee5125e4e3f33f02de7c0541a2c
KarolOrlowicz <karol.orlowicz@gmail.com>
2022-10-10 20:35:27 +0200

    added env vars to docker compose and main

--- util/secrets.py
+++ util/secrets.py
@@ -2,4 +2,8 @@ import os
 
 
 class Secrets:
-    KEY = os.environ.get("decrypt_key")
+    MYSQL_USER = os.environ.get('MYSQL_USER')
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
+    MYSQL_ROOT_PASSWORD = os.environ.get('MYSQL_ROOT_PASSWORD')
+
+
