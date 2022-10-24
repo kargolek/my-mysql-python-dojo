1ef9fc71caeed93628bf09893561c0bad8d38632
karol_orlowicz <karol.orlowicz@gmail.com>
2022-10-24 19:43:02 +0200

    adding IS NULL/IS NOT NULL operators

--- util/assertion_util.py
+++ util/assertion_util.py
@@ -1,7 +1,5 @@
-class AssertionUtil:
-
-    def is_data_contains_element(self: list, element):
-        for i in range(len(self)):
-            if self[i] == element:
-                return True
-        return False
+def is_data_contains_element(self: list, element):
+    for i in range(len(self)):
+        if self[i] == element:
+            return True
+    return False
