2eba01a9feedcba058a43460da55f085fa54c35e
KarolOrlowicz <karol.orlowicz@gmail.com>
2022-11-04 02:47:12 +0100

    add more grouping data queries
    add GROUP BY and JOIN with ordered values

--- tests/test_grouping_data.py
+++ tests/test_grouping_data.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from util.connector_util import fetch_data_print
 
 
@@ -12,3 +14,67 @@ class TestGroupingData:
                                         'GROUP BY employeeId;')
 
         assert data[0] == ('Buck', 'Sven')
+
+    # MySQL GROUP BY clause
+    def test_should_count_orders_and_group_by_the_year(self, cursor):
+        data = fetch_data_print(cursor, 'SELECT YEAR(orderDate) as year, COUNT(*) as orders FROM SalesOrder '
+                                        'GROUP BY year;')
+
+        assert data[0] == (2006, 152)
+        assert data[1] == (2007, 408)
+        assert data[2] == (2008, 270)
+
+    # MySQL GROUP BY clause
+    def test_should_count_orders_freight_and_group_by_the_year(self, cursor):
+        data = fetch_data_print(cursor, 'SELECT YEAR(orderDate) as year, SUM(freight) as freight FROM SalesOrder '
+                                        'GROUP BY year;')
+
+        assert data[0] == (2006, Decimal('10279.87'))
+        assert data[2] == (2008, Decimal('22194.05'))
+
+    # MySQL GROUP BY clause
+    def test_should_count_shipped_sales_by_shipper_in_2006(self, cursor):
+        data = fetch_data_print(cursor, 'SELECT companyName, COUNT(*) as shipped FROM SalesOrder '
+                                        'JOIN Shipper '
+                                        'Shipper USING (shipperId) '
+                                        'WHERE YEAR(orderDate) = 2006 '
+                                        'GROUP BY companyName '
+                                        'ORDER BY shipped DESC;')
+
+        assert data[0] == ('Shipper ZHISN', 58)
+        assert data[2] == ('Shipper GVSUA', 38)
+
+    # MySQL GROUP BY clause
+    def test_should_count_shipped_sales_by_shipper_in_2007(self, cursor):
+        data = fetch_data_print(cursor, 'SELECT companyName, COUNT(*) as shipped FROM SalesOrder '
+                                        'JOIN Shipper '
+                                        'Shipper USING (shipperId) '
+                                        'WHERE YEAR(orderDate) = 2007 '
+                                        'GROUP BY companyName '
+                                        'ORDER BY shipped DESC;')
+
+        assert data[0] == ('Shipper ETYNR', 153)
+        assert data[2] == ('Shipper ZHISN', 122)
+
+    # MySQL GROUP BY clause
+    def test_should_count_shipped_sales_by_shipper_in_2008(self, cursor):
+        data = fetch_data_print(cursor, 'SELECT companyName, COUNT(*) as shipped FROM SalesOrder '
+                                        'JOIN Shipper '
+                                        'Shipper USING (shipperId) '
+                                        'WHERE YEAR(orderDate) = 2008 '
+                                        'GROUP BY companyName '
+                                        'ORDER BY shipped DESC;')
+
+        assert data[0] == ('Shipper ETYNR', 117)
+        assert data[2] == ('Shipper ZHISN', 75)
+
+    # MySQL GROUP BY clause
+    def test_should_count_shipped_sales_by_shipper_all_time(self, cursor):
+        data = fetch_data_print(cursor, 'SELECT companyName, COUNT(*) as shipped FROM SalesOrder '
+                                        'JOIN Shipper '
+                                        'Shipper USING (shipperId) '
+                                        'GROUP BY companyName '
+                                        'ORDER BY shipped DESC;')
+
+        assert data[0] == ('Shipper ETYNR', 326)
+        assert data[2] == ('Shipper GVSUA', 249)
