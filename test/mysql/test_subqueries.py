ccbc9992bcd2ea01d110123e758dfd68be86f4ba
KarolOrlowicz <karol.orlowicz@gmail.com>
2022-11-08 23:10:47 +0100

    add subquery example
    1. With JOIN clause used multiple times

--- test/mysql/test_subqueries.py
+++ test/mysql/test_subqueries.py
@@ -8,9 +8,9 @@ class TestSubQueries:
     # MySQL Subquery
     def test_select_employment_age_when_hired(self, cursor):
         data = fetch_data_print(cursor, 'SELECT * FROM '
-                                        '(SELECT employeeId, firstName, lastName, '
-                                        'FLOOR((DATEDIFF(hireDate, birthDate) / 365)) as employmentAge '
-                                        'FROM Employee '
+                                        '   (SELECT employeeId, firstName, lastName, '
+                                        '   FLOOR((DATEDIFF(hireDate, birthDate) / 365)) as employmentAge '
+                                        '   FROM Employee '
                                         'ORDER BY employmentAge DESC) AS EmpDate;')
 
         assert data[0] == (4, 'Yael', 'Peled', 55)
@@ -21,9 +21,9 @@ class TestSubQueries:
         data = fetch_data_print(cursor, 'SELECT MAX(employmentAge) as maxAge, '
                                         'MIN(employmentAge) as minAge,'
                                         'AVG(employmentAge) FROM '
-                                        '(SELECT employeeId, firstName, lastName, '
-                                        'FLOOR((DATEDIFF(hireDate, birthDate) / 365)) as employmentAge '
-                                        'FROM Employee '
+                                        '   (SELECT employeeId, firstName, lastName, '
+                                        '   FLOOR((DATEDIFF(hireDate, birthDate) / 365)) as employmentAge '
+                                        '   FROM Employee '
                                         'ORDER BY employmentAge DESC) AS EmpDate;')
 
         assert data[0] == (55, 28, Decimal('36.7778'))
@@ -32,7 +32,7 @@ class TestSubQueries:
     def test_select_product_where_price_bigger_than_avg_order_price(self, cursor):
         data = fetch_data_print(cursor, 'SELECT productId, productName, unitPrice FROM Product '
                                         'WHERE unitPrice > '
-                                        '(SELECT AVG(unitPrice) FROM OrderDetail);')
+                                        '   (SELECT AVG(unitPrice) FROM OrderDetail);')
 
         assert data[0] == (7, 'Product HMLNI', Decimal('30.00'))
         assert data[25] == (72, 'Product GEEOO', Decimal('34.80'))
@@ -42,10 +42,26 @@ class TestSubQueries:
         data = fetch_data_print(cursor, 'SELECT custId, contactName, companyName, country, phone '
                                         'FROM Customer '
                                         'WHERE EXISTS( SELECT custId FROM '
-                                        '(SELECT custId, COUNT(*) as orders FROM SalesOrder '
-                                        'GROUP BY custId) AS CustomerOrders '
+                                        '   (SELECT custId, COUNT(*) as orders FROM SalesOrder '
+                                        '   GROUP BY custId) AS CustomerOrders '
                                         'WHERE orders > 15 '
                                         'AND Customer.custId = CustomerOrders.custId);')
 
         assert data[0] == (5, 'Higginbotham, Tom', 'Customer HGVLZ', 'Sweden', '0921-67 89 01')
         assert data[8] == (71, 'Navarro, Tomás', 'Customer LCOUJ', 'USA', '(208) 555-0116')
+
+    # MySQL Subquery
+    def test_select_employees_who_sell_most_products(self, cursor):
+        data = fetch_data_print(cursor, 'SELECT employeeId, firstName, lastName, ordersQuantity, totalPrice FROM '
+                                        '   (SELECT employeeId, '
+                                        '   SUM(quantity) AS ordersQuantity, '
+                                        '   SUM(unitPrice * quantity) as totalPrice '
+                                        '   FROM OrderDetail '
+                                        '   INNER JOIN SalesOrder USING (orderId) '
+                                        '   INNER JOIN Employee USING (employeeId) '
+                                        '   GROUP BY employeeId) as empQuantity '
+                                        'INNER JOIN Employee USING (employeeId) '
+                                        'ORDER BY ordersQuantity DESC;')
+
+        assert data[0] == (4, 'Yael', 'Peled', Decimal('9798'), Decimal('250187.45'))
+        assert data[8] == (9, 'Zoya', 'Dolgopyatova', Decimal('2670'), Decimal('82964.00'))
