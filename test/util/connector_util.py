6d2f87d8385fe76925f6a7d4a6f4e1345d8dd69c
KarolOrlowicz <karol.orlowicz@gmail.com>
2022-11-10 03:20:51 +0100

    clean branch
    1. remove unnecessary util folder

--- test/util/connector_util.py
+++ test/util/connector_util.py
@@ -1,4 +1,7 @@
-def print_query_data(cursor, query: str, data: list):
+from mysql.connector.cursor_cext import CMySQLCursor
+
+
+def print_query_data(cursor: CMySQLCursor, query: str, data: list):
     print(f'\nSQL query: {query}')
     print(f'{cursor.column_names}')
     if len(data) > 0:
@@ -8,7 +11,7 @@ def print_query_data(cursor, query: str, data: list):
     print(f'Size: {len(data)}')
 
 
-def fetch_data_print(cursor, query: str):
+def fetch_data_print(cursor: CMySQLCursor, query: str):
     cursor.execute(query)
     data = cursor.fetchall()
     print_query_data(cursor, query, data)
